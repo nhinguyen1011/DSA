c2b71917f4c3e10f3129857277b7b3fea3b89b35
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2022-11-29 21:04:53 +0700

    Update algorithms.cpp

--- SOURCE/algorithms.cpp
+++ SOURCE/algorithms.cpp
@@ -12,12 +12,30 @@ void swap(int &a, int &b)
 // 1.SelectionSort
 void selectionSortWithComparison(int a[], int n, unsigned long long &count_compare)
 {
-   
+   int j, min;
+    for (int i = 0; ++count_compare && i < n; i++)
+    {
+        min = i;
+        for (j = i + 1; ++count_compare && j < n; j++)
+            if (++count_compare && a[j] < a[min])
+            {
+                swap(a[min], a[j]);
+            }
+    }
 }
 
 void selectionSortWithTime(int a[], int n)
 {
-    
+    int j, min;
+    for (int i = 0; i < n; i++)
+    {
+        min = i;
+        for (j = i + 1; j < n; j++)
+            if (a[j] < a[min])
+            {
+                swap(a[i], a[j]);
+            }
+    }
 }
 
 // 2. InsertionSort
@@ -175,7 +193,7 @@ void heapSortWithComparison(int a[], int n, unsigned long long &compareCountHeap
     }
 }
 
-// 6. QuickSort
+// 5. QuickSort
 int partitionTime(int *arr, int low, int high)
 {
     
@@ -196,28 +214,123 @@ void quickSortWithComparison(int *arr, int low, int high, unsigned long long &co
     
 }
 
-// 7.MergeSort
+// 6.MergeSort
 void mergeWithTime(int array[], int const left, int const mid, int const right)
 {
-    
+    auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
+
+    auto *leftArray = new int[subArrayOne],
+         *rightArray = new int[subArrayTwo];
+
+    for (auto i = 0; i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+
+    auto indexOfSubArrayOne = 0,
+         indexOfSubArrayTwo = 0;
+    int indexOfMergedArray = left;
+
+    while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo < subArrayTwo)
+    {
+        if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo])
+        {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else
+        {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+
+    while (indexOfSubArrayOne < subArrayOne)
+    {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+
+    while (indexOfSubArrayTwo < subArrayTwo)
+    {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
 }
 
 void mergeSortWithTime(int array[], int const begin, int const end)
 {
-   
+    if (begin >= end)
+        return;
+
+    auto mid = begin + (end - begin) / 2;
+    mergeSortWithTime(array, begin, mid);
+    mergeSortWithTime(array, mid + 1, end);
+    mergeWithTime(array, begin, mid, end);
 }
 
 void mergeWithComparison(int array[], int const left, int const mid, int const right, unsigned long long &count_compare)
 {
-    
+    auto const subArrayOne = mid - left + 1;
+    auto const subArrayTwo = right - mid;
+
+    auto *leftArray = new int[subArrayOne],
+         *rightArray = new int[subArrayTwo];
+
+    for (auto i = 0; ++count_compare && i < subArrayOne; i++)
+        leftArray[i] = array[left + i];
+    for (auto j = 0; ++count_compare && j < subArrayTwo; j++)
+        rightArray[j] = array[mid + 1 + j];
+
+    auto indexOfSubArrayOne = 0,
+         indexOfSubArrayTwo = 0;
+    int indexOfMergedArray = left;
+
+    while (++count_compare && indexOfSubArrayOne < subArrayOne && ++count_compare && indexOfSubArrayTwo < subArrayTwo)
+    {
+        if (++count_compare && leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo])
+        {
+            array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+            indexOfSubArrayOne++;
+        }
+        else
+        {
+            array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+            indexOfSubArrayTwo++;
+        }
+        indexOfMergedArray++;
+    }
+
+    while (++count_compare && indexOfSubArrayOne < subArrayOne)
+    {
+        array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+        indexOfSubArrayOne++;
+        indexOfMergedArray++;
+    }
+
+    while (++count_compare && indexOfSubArrayTwo < subArrayTwo)
+    {
+        array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+        indexOfSubArrayTwo++;
+        indexOfMergedArray++;
+    }
 }
 
 void mergeSortWithComparison(int array[], int const begin, int const end, unsigned long long &count_compare)
 {
-   
-}
+    if (++count_compare && begin >= end)
+        return;
 
-// 9.RadixSort
+    auto mid = begin + (end - begin) / 2;
+    mergeSortWithComparison(array, begin, mid, count_compare);
+    mergeSortWithComparison(array, mid + 1, end, count_compare);
+    mergeWithComparison(array, begin, mid, end, count_compare);
+}
+// 7.RadixSort
 
 void radixSortWithComparison(int a[], int n, unsigned long long &count_compare)
 {
