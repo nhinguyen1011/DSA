7dc225d4caf1ca1768fefd18ab76f3752e48388b
dadikas <73420676+dadikas@users.noreply.github.com>
2022-11-29 07:13:42 +0700

    Fix

--- SOURCE/algorithms.cpp
+++ SOURCE/algorithms.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 void swap(int &a, int &b)
@@ -171,80 +172,54 @@ void mergeSortWithComparison(int array[], int const begin, int const end, unsign
 }
 
 // 9.RadixSort
-int MaxNumber(int a[], int n, unsigned long long &count_compare)
-{
-    int m = a[0];
-    count_compare = 0;
-    for (int i = 1; ++count_compare && i < n; i++)
-        if (++count_compare && a[i] > m)
-            m = a[i];
-    return m;
-}
-
-void Count(int arr[], int n, int exp, unsigned long long &count_compare)
-{
-    int bucket[n];
-    int i, box[10] = {0};
-    count_compare = 0;
-
-    for (i = 0; ++count_compare && i < n; i++)
-        box[(arr[i] / exp) % 10]++;
-
-    for (i = 1; ++count_compare && i < 10; i++)
-        box[i] += box[i - 1];
-
-    for (i = n - 1; ++count_compare && i >= 0; i--)
-    {
-        bucket[box[(arr[i] / exp) % 10] - 1] = arr[i];
-        box[(arr[i] / exp) % 10]--;
-    }
-
-    for (i = 0; ++count_compare && i < n; i++)
-        arr[i] = bucket[i];
-}
 
 void radixSortWithComparison(int a[], int n, unsigned long long &count_compare)
 {
-    int m = MaxNumber(a, n, count_compare);
-
-    for (int exp = 1; m / exp > 0; exp *= 10)
-        Count(a, n, exp, count_compare);
+    const int radix = 10;
+	int digits = 10;
+	queue<long> queues[radix];
+	
+	for(int i = 0, factor = 1; ++count_compare && i < digits; factor *= radix, i++)
+	{
+		for(int j = 0;++count_compare && j < n; j++)
+			queues[((a[j]/factor)%radix)].push(a[j]);
+			
+		int k = 0;
+		
+		for(int j = 0;++count_compare && j < radix; j++)
+		{
+			while(++count_compare && !queues[j].empty())
+			{
+				a[k] = queues[j].front();
+				queues[j].pop();
+				k++;
+			}
+		}
+	}
 }
 
-int MaxNumber1(int a[], int n)
-{
-    int m = a[0];
-    for (int i = 1; i < n; i++)
-        if (a[i] > m)
-            m = a[i];
-    return m;
-}
-
-void Count1(int arr[], int n, int exp)
-{
-    int bucket[n];
-    int i, box[10] = {0};
-
-    for (i = 0; i < n; i++)
-        box[(arr[i] / exp) % 10]++;
-
-    for (i = 1; i < 10; i++)
-        box[i] += box[i - 1];
-
-    for (i = n - 1; i >= 0; i--)
-    {
-        bucket[box[(arr[i] / exp) % 10] - 1] = arr[i];
-        box[(arr[i] / exp) % 10]--;
-    }
-
-    for (i = 0; i < n; i++)
-        arr[i] = bucket[i];
-}
 
 void radixSortWithTime(int a[], int n)
 {
-    int m = MaxNumber1(a, n);
-
-    for (int exp = 1; m / exp > 0; exp *= 10)
-        Count1(a, n, exp);
+    const int radix = 10;
+	int digits = 10;
+	queue<long> queues[radix];
+	
+	for(int i = 0, factor = 1; i < digits; factor *= radix, i++)
+	{
+		for(int j = 0; j < n; j++)
+			queues[((a[j]/factor)%radix)].push(a[j]);
+			
+		int k = 0;
+		
+		for(int j = 0; j < radix; j++)
+		{
+			while(!queues[j].empty())
+			{
+				a[k] = queues[j].front();
+				queues[j].pop();
+				k++;
+			}
+		}
+	}
 }
